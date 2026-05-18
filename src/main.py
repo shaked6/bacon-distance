88440edbb78c7955b80567c6aeb3c43cc90d07f4
shaked6 <shakedcmh@gmail.com>
2026-05-18 17:03:34 +0300

    fixed db dir

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import uvicorn
 
+from src.consts import DB_DIR
 from src.data_accessors.json_db_accessor import JsonDBAccessor
 from src.fastapi_app import create_app
 
-DB_PATH = "data/actors.json"
+DB_PATH = f"{DB_DIR}/actors.json"
 
 db_accessor = JsonDBAccessor(DB_PATH)
 app = create_app(db_accessor)
